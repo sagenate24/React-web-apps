a82b4e4457e94066eeef77da8c1cd1d2236495c7
Nate Sage <davidsage@Davids-MacBook-Pro.local>
2018-04-14 17:22:43 -0700

    Set up search bar for Yelp api and added star ratings compnonet

---
+++ ravenous/src/components/Star/StarRating.js
@@ -0,0 +1,108 @@
+import React from 'react';
+
+class Stars extends React.Component {
+    renderStars() {
+        if (this.props.business.rating === 1) {
+            return (
+                <div>
+                    <span className="fa fa-star checked"></span>
+                    <span className="fa fa-star-o checked"></span>
+                    <span className="fa fa-star-o checked"></span>
+                    <span className="fa fa-star-o checked"></span>
+                    <span className="fa fa-star-o checked"></span>
+                </div>
+            );
+        } if (this.props.business.rating === 1.5) {
+            return (
+                <div>
+                    <span className="fa fa-star checked"></span>
+                    <span className="fa fa-star-half-o checked"></span>
+                    <span className="fa fa-star-o checked"></span>
+                    <span className="fa fa-star-o checked"></span>
+                    <span className="fa fa-star-o checked"></span>
+                </div>
+            );
+        } if (this.props.business.rating === 2) {
+            return (
+                <div>
+                    <span className="fa fa-star checked"></span>
+                    <span className="fa fa-star checked"></span>
+                    <span className="fa fa-star-o checked"></span>
+                    <span className="fa fa-star-o checked"></span>
+                    <span className="fa fa-star-o checked"></span>
+                </div>
+            );
+        } if (this.props.business.rating === 2.5) {
+            return (
+                <div>
+                    <span className="fa fa-star checked"></span>
+                    <span className="fa fa-star checked"></span>
+                    <span className="fa fa-star-half-o checked"></span>
+                    <span className="fa fa-star-o checked"></span>
+                    <span className="fa fa-star-o checked"></span>
+                </div>
+            );
+        } if (this.props.business.rating === 3) {
+            return (
+                <div>
+                    <span className="fa fa-star checked"></span>
+                    <span className="fa fa-star checked"></span>
+                    <span className="fa fa-star checked"></span>
+                    <span className="fa fa-star-o checked"></span>
+                    <span className="fa fa-star-o checked"></span>
+                </div>
+            );
+        } if (this.props.business.rating === 3.5) {
+            return (
+                <div>
+                    <span className="fa fa-star checked"></span>
+                    <span className="fa fa-star checked"></span>
+                    <span className="fa fa-star checked"></span>
+                    <span className="fa fa-star-half-o checked"></span>
+                    <span className="fa fa-star-o checked"></span>
+                </div>
+            );
+        } if (this.props.business.rating === 4) {
+            return (
+                <div>
+                    <span className="fa fa-star checked"></span>
+                    <span className="fa fa-star checked"></span>
+                    <span className="fa fa-star checked"></span>
+                    <span className="fa fa-star checked"></span>
+                    <span className="fa fa-star-o checked"></span>
+                </div>
+            );
+        } if (this.props.business.rating === 4.5) {
+            return (
+                <div>
+                    <span className="fa fa-star checked"></span>
+                    <span className="fa fa-star checked"></span>
+                    <span className="fa fa-star checked"></span>
+                    <span className="fa fa-star checked"></span>
+                    <span className="fa fa-star-half-o checked"></span>
+                </div>
+            );
+        } if (this.props.business.rating === 5) {
+            return (
+                <div>
+                    <span className="fa fa-star checked"></span>
+                    <span className="fa fa-star checked"></span>
+                    <span className="fa fa-star checked"></span>
+                    <span className="fa fa-star checked"></span>
+                    <span className="fa fa-star checked"></span>
+                </div>
+            );
+        } else {
+            return 'Rating Uknown';
+        }
+    }
+
+    render() {
+        return (
+            <div className="rating">
+                {this.renderStars()}
+            </div>
+        )
+    };
+}
+export default Stars;
